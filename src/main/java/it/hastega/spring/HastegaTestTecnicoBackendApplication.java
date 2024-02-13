c6eed6f85438b3246278a68a6ae2791718cc908c
marcobusacca <marcobusacca01@gmail.com>
2024-02-13 13:03:45 +0100

    first commit

---
+++ src/main/java/it/hastega/spring/HastegaTestTecnicoBackendApplication.java
@@ -0,0 +1,13 @@
+package it.hastega.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastegaTestTecnicoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HastegaTestTecnicoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/hastega/spring/HastegaTestTecnicoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.hastega.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastegaTestTecnicoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
