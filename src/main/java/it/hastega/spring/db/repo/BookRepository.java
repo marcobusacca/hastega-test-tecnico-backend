9890dc59867c8b6f82e5f632e5832fb822bbc2d0
marcobusacca <marcobusacca01@gmail.com>
2024-02-14 10:47:14 +0100

    [FEAT] Definizione Service "BookService"

--- src/main/java/it/hastega/spring/db/repo/BookRepository.java
+++ src/main/java/it/hastega/spring/db/repo/BookRepository.java
@@ -1,9 +1,11 @@
 package it.hastega.spring.db.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import it.hastega.spring.db.pojo.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
 }
---
+++ src/main/java/it/hastega/spring/db/serv/BookService.java
@@ -0,0 +1,32 @@
+package it.hastega.spring.db.serv;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.hastega.spring.db.pojo.Book;
+import it.hastega.spring.db.repo.BookRepository;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book findById(int id) {
+        return bookRepository.findById(id).get();
+    }
+
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+}
